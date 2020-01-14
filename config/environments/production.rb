a4e81a79a270294e2d4a027686af4d4eda710be5
yusuke nakano <yuusukechrono@gmail.com>
2020-01-14 15:41:35 +0000

    production.rbを参考ページで修正２

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'safe-coast-38337.herokuapp.com' }
 
-  config.action_mailder.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
